1b4b41089eeaf9bb4a9cfdd03d61470858fd259a
Jesús Rodríguez <jesus.jarp@gmail.com>
2022-08-11 21:38:26 +0300

    Adding basic structure, and mongoose setup

--- api-superflights/src/main.ts
+++ api-superflights/src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
+import { AllExceptionFilter } from './common/filters/http-exception.filter';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptors';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new AllExceptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
